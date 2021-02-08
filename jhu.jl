ed6b1dff367c4b5c6cd596015a597944f3e1d93a
Hiroyuki Chihara <77265300+hiroyuki-chihara@users.noreply.github.com>
2021-02-08 12:26:39 +0900

    Add files via upload

---
+++ jhu.jl
@@ -0,0 +1,416 @@
+# jhu.jl
+#######################
+
+# Load packages. 
+using Plots, CSV, Dates, HTTP, DataFrames, DifferentialEquations
+
+# Download data from the MHLW web site. 
+download("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv","time_series_covid19_confirmed_global.csv");
+download("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv","time_series_covid19_deaths_global.csv");
+download("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv","time_series_covid19_recovered_global.csv");
+
+Acsv=DataFrame(CSV.File("time_series_covid19_confirmed_global.csv", header=false, delim=','));
+Bcsv=DataFrame(CSV.File("time_series_covid19_deaths_global.csv", header=false, delim=','));
+Ccsv=DataFrame(CSV.File("time_series_covid19_recovered_global.csv", header=false, delim=','));
+(pa,qa)=size(Acsv);
+
+# Japan
+PJPN=126;
+AJPN=parse.(Float64,Array(Acsv[156,5:qa]))/PJPN;
+BJPN=parse.(Float64,Array(Bcsv[156,5:qa]))/PJPN;
+CJPN=parse.(Float64,Array(Acsv[156,5:qa]));
+RJPN=parse.(Float64,Array(Ccsv[141,5:qa]));
+IJPN=CJPN-RJPN;
+# South Korea
+PKOR=51.3;
+AKOR=parse.(Float64,Array(Acsv[160,5:qa]))/PKOR;
+BKOR=parse.(Float64,Array(Bcsv[160,5:qa]))/PKOR;
+CKOR=parse.(Float64,Array(Acsv[160,5:qa]));
+RKOR=parse.(Float64,Array(Ccsv[145,5:qa]));
+IKOR=CKOR-RKOR;
+# Philippines
+PPHI=108;
+APHI=parse.(Float64,Array(Acsv[210,5:qa]))/PPHI;
+BPHI=parse.(Float64,Array(Bcsv[210,5:qa]))/PPHI;
+CPHI=parse.(Float64,Array(Acsv[210,5:qa]));
+RPHI=parse.(Float64,Array(Ccsv[195,5:qa]));
+IPHI=CPHI-RPHI;
+# Malaysia
+PMYS=32;
+AMYS=parse.(Float64,Array(Acsv[176,5:qa]))/PMYS;
+BMYS=parse.(Float64,Array(Bcsv[176,5:qa]))/PMYS;
+CMYS=parse.(Float64,Array(Acsv[176,5:qa]));
+RMYS=parse.(Float64,Array(Ccsv[161,5:qa]));
+IMYS=CMYS-RMYS;
+# Indonesia
+PIND=271;
+AIND=parse.(Float64,Array(Acsv[149,5:qa]))/PIND;
+BIND=parse.(Float64,Array(Bcsv[149,5:qa]))/PIND;
+CIND=parse.(Float64,Array(Acsv[149,5:qa]));
+RIND=parse.(Float64,Array(Ccsv[134,5:qa]));
+IIND=CIND-RIND;
+# Singapore
+PSIN=5.7;
+ASIN=parse.(Float64,Array(Acsv[228,5:qa]))/PSIN;
+BSIN=parse.(Float64,Array(Bcsv[228,5:qa]))/PSIN;
+CSIN=parse.(Float64,Array(Acsv[228,5:qa]));
+RSIN=parse.(Float64,Array(Ccsv[213,5:qa]));
+ISIN=CSIN-RSIN;
+# New Zealand
+PNZL=4.9;
+ANZL=parse.(Float64,Array(Acsv[198,5:qa]))/PNZL;
+BNZL=parse.(Float64,Array(Bcsv[198,5:qa]))/PNZL;
+CNZL=parse.(Float64,Array(Acsv[198,5:qa]));
+RNZL=parse.(Float64,Array(Ccsv[183,5:qa]));
+INZL=CNZL-RNZL;
+# Hubei
+PHubei=58.5;
+AHubei=parse.(Float64,Array(Acsv[73,5:qa]))/PHubei;
+BHubei=parse.(Float64,Array(Bcsv[73,5:qa]))/PHubei;
+CHubei=parse.(Float64,Array(Acsv[73,5:qa]));
+RHubei=parse.(Float64,Array(Ccsv[58,5:qa]));
+IHubei=CHubei-RHubei;
+# Victoria
+PVIC=6.4;
+AVIC=parse.(Float64,Array(Acsv[16,5:qa]))/PVIC;
+BVIC=parse.(Float64,Array(Bcsv[16,5:qa]))/PVIC;
+CVIC=parse.(Float64,Array(Acsv[16,5:qa]));
+RVIC=parse.(Float64,Array(Ccsv[16,5:qa]));
+IVIC=CVIC-RVIC;
+# Victoria
+PNSW=7.5;
+ANSW=parse.(Float64,Array(Acsv[11,5:qa]))/PNSW;
+BNSW=parse.(Float64,Array(Bcsv[11,5:qa]))/PNSW;
+CNSW=parse.(Float64,Array(Acsv[11,5:qa]));
+RNSW=parse.(Float64,Array(Ccsv[11,5:qa]));
+INSW=CNSW-RNSW;
+
+D=qa-4;
+d0=Date(2020,1,22);
+d1=d0+Day(D-1);
+l0=string(d0);
+l1=string(d0+Day(Int(floor((D-1)/4))));
+l2=string(d0+Day(Int(floor((D-1)/2))));
+l3=string(d0+Day(Int(floor(3*(D-1)/4))));
+l4=string(d0+Day(D-1));
+
+plot([AJPN APHI AMYS AIND AKOR],  
+    grid=false,
+    linewidth=3, 
+    title="COVID-19 in Asia: Confirmed cases per 1M \n data sourced by JHU Coronavirus Resource Center", 
+    xlabel="date",
+    yaxis="cases/1M",
+    legendfont=font(14), 
+    label=["Japan" "Philippines" "Malaysia" "Indonesia" "South Korea"],
+    legend = :topleft)
+plot!(xticks = ([1 floor(D/4)  floor(D/2) floor(3*D/4) D-3;], [l0 l1 l2 l3 l4]))
+savefig("jhu_cases.png") 
+
+plot([BJPN BPHI BMYS BIND BKOR], 
+    grid=false,
+    linewidth=3, 
+    title="COVID-19 in Asia: Deaths per 1M \n data sourced by JHU Coronavirus Resource Center", 
+    xlabel="date",
+    yaxis="deaths/1M",
+    legendfont=font(14), 
+    label=["Japan" "Philippines" "Malaysia" "Indonesia" "South Korea"],
+    legend = :topleft)
+plot!(xticks = ([1 floor(D/4)  floor(D/2) floor(3*D/4) D-3;], [l0 l1 l2 l3 l4]))
+savefig("jhu_deaths.png") 
+
+plot([CJPN IJPN RJPN], 
+    grid=false,
+    linewidth=3, 
+    title="COVID-19 in Japan (126M) \n data sourced by JHU Coronavirus Resource Center", 
+    xlabel="date",
+    yaxis="cases",
+    legendfont=font(14), 
+    label=["total cases" "active cases" "discharged"], 
+    legend = :topleft)
+plot!(xticks = ([1 floor(D/4)  floor(D/2) floor(3*D/4) D-3;], [l0 l1 l2 l3 l4]))
+savefig("jhu_japan.png") 
+
+plot([CKOR IKOR RKOR], 
+    grid=false,
+    linewidth=3, 
+    title="COVID-19 in South Korea (51.3M) \n data sourced by JHU Coronavirus Resource Center", 
+    xlabel="date",
+    yaxis="cases",
+    legendfont=font(14), 
+    label=["total cases" "active cases" "discharged"], 
+    legend = :topleft)
+plot!(xticks = ([1 floor(D/4)  floor(D/2) floor(3*D/4) D-3;], [l0 l1 l2 l3 l4]))
+savefig("jhu_south_korea.png")
+
+plot([CPHI IPHI RPHI], 
+    grid=false,
+    linewidth=3, 
+    title="COVID-19 in Philippines (108M) \n data sourced by JHU Coronavirus Resource Center", 
+    xlabel="date",
+    yaxis="cases",
+    legendfont=font(14), 
+    label=["total cases" "active cases" "discharged"], 
+    legend = :topleft)
+plot!(xticks = ([1 floor(D/4)  floor(D/2) floor(3*D/4) D-3;], [l0 l1 l2 l3 l4]))
+savefig("jhu_philippines.png")
+
+plot([CMYS IMYS RMYS], 
+    grid=false,
+    linewidth=3, 
+    title="COVID-19 in Malaysia (32M) \n data sourced by JHU Coronavirus Resource Center", 
+    xlabel="date",
+    yaxis="cases",
+    legendfont=font(14), 
+    label=["total cases" "active cases" "discharged"], 
+    legend = :topleft)
+plot!(xticks = ([1 floor(D/4)  floor(D/2) floor(3*D/4) D-3;], [l0 l1 l2 l3 l4]))
+savefig("jhu_malaysia.png")
+
+plot([CIND IIND RIND], 
+    grid=false,
+    linewidth=3, 
+    title="COVID-19 in Indonesia (271M) \n data sourced by JHU Coronavirus Resource Center", 
+    xlabel="date",
+    yaxis="cases",
+    legendfont=font(14), 
+    label=["total cases" "active cases" "discharged"], 
+    legend = :topleft)
+plot!(xticks = ([1 floor(D/4)  floor(D/2) floor(3*D/4) D-3;], [l0 l1 l2 l3 l4]))
+savefig("jhu_indonesia.png")
+
+plot([CSIN ISIN RSIN], 
+    grid=false,
+    linewidth=3, 
+    title="COVID-19 in Singapore (5.7M) \n data sourced by JHU Coronavirus Resource Center", 
+    xlabel="date",
+    yaxis="cases",
+    legendfont=font(14), 
+    label=["total cases" "active cases" "discharged"], 
+    legend = :right)
+plot!(xticks = ([1 floor(D/4)  floor(D/2) floor(3*D/4) D-3;], [l0 l1 l2 l3 l4]))
+savefig("jhu_singapore.png")
+
+plot([CNZL INZL RNZL], 
+    grid=false,
+    linewidth=3, 
+    title="COVID-19 in New Zealand (4.9M) \n data sourced by JHU Coronavirus Resource Center", 
+    xlabel="date",
+    yaxis="cases",
+    legendfont=font(14), 
+    label=["total cases" "active cases" "discharged"], 
+    legend = :right)
+plot!(xticks = ([1 floor(D/4)  floor(D/2) floor(3*D/4) D-3;], [l0 l1 l2 l3 l4]))
+savefig("jhu_new_zealand.png")
+
+plot([CHubei IHubei RHubei], 
+    grid=false,
+    linewidth=3, 
+    title="COVID-19 in Hubei Province (58.5M) \n data sourced by JHU Coronavirus Resource Center", 
+    xlabel="date",
+    yaxis="cases",
+    legendfont=font(14), 
+    label=["total cases" "active cases" "discharged"], 
+    legend = :right)
+plot!(xticks = ([1 floor(D/4)  floor(D/2) floor(3*D/4) D-3;], [l0 l1 l2 l3 l4]))
+savefig("jhu_hubei.png")
+
+plot([CVIC IVIC RVIC], 
+    grid=false,
+    linewidth=3, 
+    title="COVID-19 in Victoria (AUS) (6.4M) \n data sourced by JHU Coronavirus Resource Center", 
+    xlabel="date",
+    yaxis="cases",
+    legendfont=font(14), 
+    label=["total cases" "active cases" "discharged"], 
+    legend = :topleft)
+plot!(xticks = ([1 floor(D/4)  floor(D/2) floor(3*D/4) D-3;], [l0 l1 l2 l3 l4]))
+savefig("jhu_victoria.png")
+
+plot([CNSW INSW RNSW], 
+    grid=false,
+    linewidth=3, 
+    title="COVID-19 in New South Wales (AUS) (7.5M) \n data sourced by JHU Coronavirus Resource Center", 
+    xlabel="date",
+    yaxis="cases",
+    legendfont=font(14), 
+    label=["total cases" "active cases" "discharged"], 
+    legend = :topleft)
+plot!(xticks = ([1 floor(D/4)  floor(D/2) floor(3*D/4) D-3;], [l0 l1 l2 l3 l4]))
+savefig("jhu_new_south_wales.png")
+
+# Japan
+A1T0=CJPN[qa-4];
+A1R0=RJPN[qa-4];
+A1T2=CJPN[qa-10];
+A1R2=RJPN[qa-10];
+A1N=PJPN;
+# South Korea
+A2T0=CKOR[qa-4];
+A2R0=RKOR[qa-4];
+A2T2=CKOR[qa-10];
+A2R2=RKOR[qa-10];
+A2N=PKOR;
+# Philippines
+A3T0=CPHI[qa-4];
+A3R0=RPHI[qa-4];
+A3T2=CPHI[qa-31];
+A3R2=RPHI[qa-31];
+A3N=PPHI;
+# Malaysia
+A4T0=CMYS[qa-4];
+A4R0=RMYS[qa-4];
+A4T2=CMYS[qa-10];
+A4R2=RMYS[qa-10];
+A4N=PMYS;
+# Indonesia
+A5T0=CIND[qa-4];
+A5R0=RIND[qa-4];
+A5T2=CIND[qa-10];
+A5R2=RIND[qa-10];
+A5N=PIND;
+
+# interval 
+W=Float64(5*7)
+w0=string(d1);
+w1=string(d1+Day(2*7));
+w2=string(d1+Day(4*7));
+        
+# Japan
+T0=A1T0;
+R0=A1R0;
+T2=A1T2;
+R2=A1R2;
+N=A1N;
+T1=(T0-T2)/Float64(7);
+R1=(R0-R2)/Float64(7);
+beta=T1/(T0-R0)/(N-T0);
+gamma=R1/(T0-R0);
+tspan=(0,W);
+f(u,p,t) = [beta*u[2]*(N-u[1]);u[2]*(beta*(N-u[1])-gamma); gamma*u[2]];
+u0=[T0;T0-R0;R0];
+prob = ODEProblem(f,u0,tspan);
+sol = solve(prob);
+# plotting
+plot(sol, 
+    grid=false,
+    linewidth=3, 
+    title="SIR model for COVID-19 in Japan (126M)", 
+    xlabel="date",
+    yaxis="cases",
+    legendfont=font(14), 
+    label=["total cases" "active cases" "discharged"], 
+    legend = :right)
+plot!(xticks = ([1 2*7 4*7;], [w0, w1, w2]))
+savefig("sir_japan.png")
+
+# South Korea
+T0=A2T0;
+R0=A2R0;
+T2=A2T2;
+R2=A2R2;
+N=A2N
+T1=(T0-T2)/Float64(7);
+R1=(R0-R2)/Float64(7);
+beta=T1/(T0-R0)/(N-T0);
+gamma=R1/(T0-R0);
+tspan=(0,W);
+f(u,p,t) = [beta*u[2]*(N-u[1]);u[2]*(beta*(N-u[1])-gamma); gamma*u[2]];
+u0=[T0;T0-R0;R0];
+prob = ODEProblem(f,u0,tspan);
+sol = solve(prob);
+# plotting
+plot(sol, 
+    grid=false,
+    linewidth=3, 
+    title="SIR model for COVID-19 in South Korea (51.3M)", 
+    xlabel="date",
+    yaxis="cases",
+    legendfont=font(14), 
+    label=["total cases" "active cases" "discharged"], 
+    legend = :right)
+plot!(xticks = ([1 2*7 4*7;], [w0, w1, w2]))
+savefig("sir_south_Korea.png")
+
+# Philippines
+T0=A3T0;
+R0=A3R0;
+T2=A3T2;
+R2=A3R2;
+N=A3N
+T1=(T0-T2)/Float64(28);
+R1=(R0-R2)/Float64(28);
+beta=T1/(T0-R0)/(N-T0);
+gamma=R1/(T0-R0);
+tspan=(0,W);
+f(u,p,t) = [beta*u[2]*(N-u[1]);u[2]*(beta*(N-u[1])-gamma); gamma*u[2]];
+u0=[T0;T0-R0;R0];
+prob = ODEProblem(f,u0,tspan);
+sol = solve(prob);
+# plotting
+plot(sol, 
+    grid=false,
+    linewidth=3, 
+    title="SIR model for COVID-19 in Philippines (108M)", 
+    xlabel="date",
+    yaxis="cases",
+    legendfont=font(14), 
+    label=["total cases" "active cases" "discharged"], 
+    legend = :right)
+plot!(xticks = ([1 2*7 4*7;], [w0, w1, w2]))
+savefig("sir_philippines.png")
+
+# Malaysia
+T0=A4T0;
+R0=A4R0;
+T2=A4T2;
+R2=A4R2;
+N=A4N
+T1=(T0-T2)/Float64(7);
+R1=(R0-R2)/Float64(7);
+beta=T1/(T0-R0)/(N-T0);
+gamma=R1/(T0-R0);
+tspan=(0,W);
+f(u,p,t) = [beta*u[2]*(N-u[1]);u[2]*(beta*(N-u[1])-gamma); gamma*u[2]];
+u0=[T0;T0-R0;R0];
+prob = ODEProblem(f,u0,tspan);
+sol = solve(prob);
+# plotting
+plot(sol, 
+    grid=false,
+    linewidth=3, 
+    title="SIR model for COVID-19 in Malaysia (32M)", 
+    xlabel="date",
+    yaxis="cases",
+    legendfont=font(14), 
+    label=["total cases" "active cases" "discharged"], 
+    legend = :topleft)
+plot!(xticks = ([1 2*7 4*7;], [w0, w1, w2]))
+savefig("sir_malaysia.png")
+
+# Indonesia
+T0=A5T0;
+R0=A5R0;
+T2=A5T2;
+R2=A5R2;
+N=A5N
+T1=(T0-T2)/Float64(7);
+R1=(R0-R2)/Float64(7);
+beta=T1/(T0-R0)/(N-T0);
+gamma=R1/(T0-R0);
+tspan=(0,W);
+f(u,p,t) = [beta*u[2]*(N-u[1]);u[2]*(beta*(N-u[1])-gamma); gamma*u[2]];
+u0=[T0;T0-R0;R0];
+prob = ODEProblem(f,u0,tspan);
+sol = solve(prob);
+# plotting
+plot(sol, 
+    grid=false,
+    linewidth=3, 
+    title="SIR model for COVID-19 in Indonesia (271M)", 
+    xlabel="date",
+    yaxis="cases",
+    legendfont=font(14), 
+    label=["total cases" "active cases" "discharged"], 
+    legend = :topleft)
+plot!(xticks = ([1 2*7 4*7;], [w0, w1, w2]))
+savefig("sir_indonesia.png")
---
+++ nhk.jl
@@ -0,0 +1,156 @@
+# nhk_data.jl
+#######################
+
+# Load packages. 
+using Plots, CSV, Dates, HTTP, DataFrames
+
+# Download data from the MHLW web site. 
+download("https://www3.nhk.or.jp/n-data/opendata/coronavirus/nhk_news_covid19_prefectures_daily_data.csv","nhk_news_covid19_prefectures_daily_data.csv");
+download("https://www3.nhk.or.jp/n-data/opendata/coronavirus/nhk_news_covid19_domestic_daily_data.csv","nhk_news_covid19_domestic_daily_data.csv");
+
+# Shape up the data.
+Acsv=DataFrame(CSV.File("nhk_news_covid19_prefectures_daily_data.csv", header=false, delim=','));
+Jcsv=DataFrame(CSV.File("nhk_news_covid19_domestic_daily_data.csv", header=false, delim=','));
+(pa,qa)=size(Acsv);
+A0=parse.(Int64,Acsv[2:pa,2]);
+A1=parse.(Float64,Acsv[2:pa,4]);
+A2=parse.(Float64,Acsv[2:pa,5]);
+A3=parse.(Float64,Acsv[2:pa,7]);
+(pj,qj)=size(Jcsv);
+J1=parse.(Float64,Jcsv[2:pj,2]);
+J2=parse.(Float64,Jcsv[2:pj,3]);
+J3=parse.(Float64,Jcsv[2:pj,5]);
+
+# Plot the data
+# d0: the initial date
+# df: the final day
+d0=Date(2020,1,16)
+D=length(J1);
+df=d0+Day(D-1);
+l0=string(d0);
+l1=string(d0+Day(Int(floor((D-1)/4))));
+l2=string(d0+Day(Int(floor((D-1)/2))));
+l3=string(d0+Day(Int(floor(3*(D-1)/4))));
+l4=string(df);
+
+# Okinawa (1.46M): code 47
+rowoknw=findall(x->x==47,A0);
+NOKNW=A1[rowoknw]/1.46;
+TOKNW=A2[rowoknw]/1.46;
+DOKNW=A3[rowoknw]/1.46;
+# Hokkaido (5.27M): code 1, 
+rowhkd=findall(x->x==1,A0);
+NHKD=A1[rowhkd]/5.27;
+THKD=A2[rowhkd]/5.27;
+DHKD=A3[rowhkd]/5.27;
+# Tokyo (14M): code 13, 
+rowtky=findall(x->x==13,A0);
+NTKY=A1[rowtky]/14;
+TTKY=A2[rowtky]/14;
+DTKY=A3[rowtky]/14;
+# Osaka (8.81M): code 27
+rowosk=findall(x->x==27,A0);
+NOSK=A1[rowosk]/8.81;
+TOSK=A2[rowosk]/8.81;
+DOSK=A3[rowosk]/8.81;
+# Japan (126M)
+NJPN=J1/126;
+TJPN=J2/126;
+DJPN=J3/126;
+# Hyogo (5.43M): code 28, 
+rowhyg=findall(x->x==28,A0);
+NHYG=A1[rowhyg]/5.43;
+THYG=A2[rowhyg]/5.43;
+DHYG=A3[rowhyg]/5.43;
+# Kyoto (2.57M): code 26
+rowkyt=findall(x->x==26,A0);
+NKYT=A1[rowkyt]/2.57;
+TKYT=A2[rowkyt]/2.57;
+DKYT=A3[rowkyt]/2.57;
+# Saitama (7.34M): code 11, 
+rowstm=findall(x->x==11,A0);
+CSTM=A1[rowstm]/7.34;
+TSTM=A2[rowstm]/7.34;
+DSTM=A3[rowstm]/7.34;
+# Chiba (6.28M): code 12
+rowchb=findall(x->x==12,A0);
+CCHB=A1[rowchb]/6.28;
+TCHB=A2[rowchb]/6.28;
+DCHB=A3[rowchb]/6.28;
+# kanagawa (9.22M): code 14
+rowkng=findall(x->x==14,A0);
+CKNG=A1[rowkng]/9.22;
+TKNG=A2[rowkng]/9.22;
+DKNG=A3[rowkng]/9.22;
+
+plot([NJPN NTKY NHKD NOSK NOKNW], 
+    grid=false,
+    linewidth=1, 
+    title="COVID-19 in Japan (daily new cases per 1M) \n data sourced by NHK", 
+    xlabel="date",
+    yaxis="cases/1M",
+    legendfont=font(14), 
+    label=["Japan" "Tokyo" "Hokkaido" "Osaka" "Okinawa"], 
+    legend = :topleft)
+plot!(xticks = ([1 floor(D/4) floor(D/2) floor(3*D/4) D-3;], [l0 l1 l2 l3 l4]))
+savefig("nhk_new_cases.png") 
+
+plot([TJPN TTKY THKD TOSK THYG TOKNW TKNG], 
+    grid=false,
+    linewidth=3, 
+    legendfont=font(14), 
+    label=["Japan" "Tokyo" "Hokkaido" "Osaka" "Hyogo" "Okinawa" "Kanagawa"], 
+    title="COVID-19 in Japan (total cases per 1M) \n data sourced by NHK", 
+    xlabel="date",
+    yaxis="cases/1M",    
+    legend = :topleft)
+plot!(xticks = ([1 floor(D/4) floor(D/2) floor(3*D/4) D-3;], [l0 l1 l2 l3 l4]))
+savefig("nhk_cases.png") 
+
+plot([DJPN DTKY DHKD DOSK DHYG DOKNW DKNG], 
+    grid=false,
+    linewidth=3, 
+    title="COVID-19 in Japan (deaths per 1M) \n data sourced by NHK", 
+    xlabel="date",
+    yaxis="deaths/1M",
+    legendfont=font(14), 
+    label=["Japan" "Tokyo" "Hokkaido" "Osaka" "Hyogo" "Okinawa" "Kanagawa"], 
+    legend = :topleft)
+plot!(xticks = ([1 floor(D/4) floor(D/2) floor(3*D/4) D-3;], [l0 l1 l2 l3 l4]))
+savefig("nhk_deaths.png") 
+
+plot([NKYT NHYG NTKY NOSK NJPN], 
+    grid=false,
+    linewidth=1, 
+    title="COVID-19 in KEIHANSHIN (daily new cases per 1M) \n data sourced by NHK", 
+    xlabel="date",
+    yaxis="cases/1M",
+    legendfont=font(14), 
+    label=["Kyoto" "Hyogo" "Tokyo" "Osaka" "Japan"], 
+    legend = :topleft)
+plot!(xticks = ([1 floor(D/4) floor(D/2) floor(3*D/4) D-3;], [l0 l1 l2 l3 l4]))
+savefig("nhk2_new_cases.png") 
+
+plot([TJPN TTKY TSTM TCHB TKYT], 
+    grid=false,
+    linewidth=3, 
+    legendfont=font(14), 
+    label=["Japan" "Tokyo" "Saitama" "Chiba" "Kyoto"], 
+    title="COVID-19 in Japan 2 (total cases per 1M) \n data sourced by NHK", 
+    xlabel="date",
+    yaxis="cases/1M",    
+    legend = :topleft)
+plot!(xticks = ([1 floor(D/4) floor(D/2) floor(3*D/4) D-3;], [l0 l1 l2 l3 l4]))
+savefig("nhk2_cases.png") 
+
+plot([DJPN DTKY DSTM DCHB DKYT], 
+    grid=false,
+    linewidth=3, 
+    title="COVID-19 in Japan 2 (deaths per 1M) \n data sourced by NHK", 
+    xlabel="date",
+    yaxis="deaths/1M",
+    legendfont=font(14), 
+    label=["Japan" "Tokyo" "Saitama" "Chiba" "Kyoto"], 
+    legend = :topleft)
+plot!(xticks = ([1 floor(D/4) floor(D/2) floor(3*D/4) D-3;], [l0 l1 l2 l3 l4]))
+savefig("nhk2_deaths.png") 
