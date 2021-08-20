65da888eedcb10cac896a2ddc246edd57687e27b
Hiroyuki Chihara <77265300+hiroyuki-chihara@users.noreply.github.com>
2021-08-21 07:28:06 +0900

    Add files via upload

--- nhk.jl
+++ nhk.jl
@@ -178,20 +178,6 @@ plot([NJPN NTKY NOKNW],
     legend = :topleft)
 savefig("nhk_new_cases_okinawa.png") 
 
-plot([NJPN NTKY NOKNW], 
-    grid=false,
-    linewidth=1, 
-    title="COVID-19 in Japan (daily new cases per 1M) \n data sourced by NHK (Japanese Public TV)", 
-    right_margin=Plots.Measures.Length(:mm, 10.0),
-    xticks = ([1 floor(D/4)  floor(D/2) floor(3*D/4) D;], [l0 l1 l2 l3 l4]),
-    xlabel="date",
-    yaxis="cases/1M",
-    legendfont=font(14), 
-    label=["Japan" "Tokyo" "Okinawa"], 
-    palette = :seaborn_bright, 
-    legend = :topleft)
-savefig("nhk_new_cases_okinawa.png") 
-
 plot([J3 J3TKY J3OSK], 
     grid=false,
     linewidth=3, 
